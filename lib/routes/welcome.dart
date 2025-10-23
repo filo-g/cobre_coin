120d9afad46f999296c4df3327d300f6cc68fb48
Filo <filogame@proton.me>
2025-10-24 01:44:37 +0200

    welcome view now shows multi accounts

--- lib/routes/welcome.dart
+++ lib/routes/welcome.dart
@@ -14,26 +14,15 @@ class WelcomeRoute extends StatefulWidget {
 
 class _WelcomeRouteState extends State<WelcomeRoute> {
 
-  int? _balance;
-  String _formattedBalance = '0';
+  final NumberFormat _formatter = NumberFormat('#,##0', 'es_ES');
+  List? _accounts = [];
   var _loading = true;
-  Future<void> _getBalance() async {
+  Future<void> _getAccounts() async {
     setState(() {
       _loading = true;
     });
     try {
-      final accounts = await SupabaseUtils.getUserAccounts();
-      context.showSnackBar('$accounts', isError: false);
-      if (accounts != null && accounts.isNotEmpty) {
-        final balanceValue = accounts.first['balance'];
-
-        if (balanceValue is num) {
-          _balance = balanceValue.toInt();
-        } else {
-          _balance = 0;
-        }
-        _formattedBalance = NumberFormat('#,##0', 'es_ES').format(_balance);
-      }
+      _accounts = await SupabaseUtils.getUserAccounts();
     } catch (error) {
       if (mounted) {
         context.showSnackBar('$error', isError: true);
@@ -46,11 +35,11 @@ class _WelcomeRouteState extends State<WelcomeRoute> {
       }
     }
   }
-  
+
   @override
   void initState() {
     super.initState();
-    _getBalance();
+    _getAccounts();
   }
 
   @override
@@ -59,63 +48,74 @@ class _WelcomeRouteState extends State<WelcomeRoute> {
 
     return LayoutBuilder(
       builder: (context, constraints) {
-        final cardWidth = constraints.maxWidth * 0.8; // 80% of screen width
+        final cardWidth = constraints.maxWidth * 0.8;
         final cardPadding = constraints.maxWidth < 400 ? 16.0 : 24.0;
 
         return Center(
-          child: Container(
-            padding: EdgeInsets.all(cardPadding),
+          child: SizedBox(
             width: cardWidth,
-            decoration: BoxDecoration(
-              color: theme.colorScheme.surfaceBright,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withValues(alpha: 0.7),
-                  blurRadius: 8,
-                  offset: const Offset(0, 4),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Balance',
-                  style: TextStyle(
-                    color: theme.colorScheme.secondary,
-                    fontSize: constraints.maxWidth < 400 ? 14 : 16,
-                    fontWeight: FontWeight.w500,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: _accounts?.length,
+              itemBuilder: (context, index) {
+                final account = _accounts![index];
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 12),
+                  width: cardWidth,
+                  padding: EdgeInsets.all(cardPadding),
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surfaceBright,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.3),
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        _formattedBalance,
-                        overflow: TextOverflow.ellipsis,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Balance - ${account['name']}',
                         style: TextStyle(
-                          color: theme.colorScheme.primary,
-                          fontSize: constraints.maxWidth < 400 ? 24 : 28,
-                          fontWeight: FontWeight.bold,
+                          color: theme.colorScheme.secondary,
+                          fontSize: constraints.maxWidth < 400 ? 14 : 16,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 6),
-                    SvgPicture.asset(
-                      'assets/svg/cobre.svg',
-                      height: constraints.maxWidth < 400 ? 24 : 28,
-                      colorFilter: ColorFilter.mode(
-                        theme.colorScheme.primary,
-                        BlendMode.srcIn,
+                      const SizedBox(height: 8),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              _formatter.format(account['balance']),
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: theme.colorScheme.primary,
+                                fontSize: constraints.maxWidth < 400 ? 24 : 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 6),
+                          SvgPicture.asset(
+                            'assets/svg/cobre.svg',
+                            height: constraints.maxWidth < 400 ? 24 : 28,
+                            colorFilter: ColorFilter.mode(
+                              theme.colorScheme.primary,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         );
