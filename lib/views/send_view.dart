a207e45851407f30b848c042f44240d061a63cca
Filo <filogame@proton.me>
2025-11-01 20:05:35 +0100

    send view - user list (not tested as there are no more users)

--- lib/views/send_view.dart
+++ lib/views/send_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cobre_coin/utils/supabase_utils.dart';
 
 class SendView extends StatefulWidget {
   const SendView({super.key});
@@ -8,53 +9,70 @@ class SendView extends StatefulWidget {
 }
 
 class _SendViewState extends State<SendView> {
-  final List<String> _items = List.generate(30, (i) => 'Item ${i + 1}');
+  bool _isLoading = true;
+  late final List<Map<String, dynamic>>? _users;
 
-  void _onFabPressed() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Floating Action Button pressed!')),
+  Future<void> _getUsers() async {
+    final users = await SupabaseUtils.getUsers();
+    setState(() {
+      _users = users;
+      _isLoading = false;
+    });
+  }
+
+  void _showUserInfo(Map<String, dynamic> user) {
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return StatefulBuilder(
+          builder: (
+            BuildContext context,
+            void Function(void Function()) setStateDialog
+          ) {
+            return AlertDialog();
+          }
+        );
+      },
     );
   }
 
   @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search View'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ListView.builder(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-          itemCount: _items.length,
-          itemBuilder: (context, index) {
-            return Card(
-              margin: const EdgeInsets.symmetric(vertical: 8),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              elevation: 2,
-              child: ListTile(
-                leading: const Icon(Icons.label_outline),
-                title: Text(
-                  _items[index],
-                  style: const TextStyle(fontSize: 18),
-                ),
-                trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-                onTap: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Tapped ${_items[index]}')),
-                  );
-                },
-              ),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _onFabPressed,
-        child: const Icon(Icons.add),
-      ),
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    }
+
+    if (_users == null || _users.isEmpty) {
+      return Center(
+        child: Text('No users found.')
+      );
+    }
+
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: _users.length,
+      itemBuilder: (context, index) {
+        final user = _users[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 6),
+          child: ListTile(
+            title: Text(
+              '${user['display_name'] ?? 'no one'})'
+            ),
+            subtitle: Text(
+              '@${user['username'] ?? 'e404'}'
+            ),
+          ),
+        );
+      },
     );
   }
 }
